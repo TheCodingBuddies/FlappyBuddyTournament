811ba65161820991dce43766b0ccf82223d25c07
CB_Fabi <fabian@codingbuddies.de>
2025-01-13 16:13:20 +0100

    Use up to date method. Handle server shut down

--- bots/pythonClient/bot.py
+++ bots/pythonClient/bot.py
@@ -8,28 +8,39 @@ from Bots.aiFactory import ai_factory
 from Bots.data import PlayState
 
 
-async def client(ai_bot, port):
+async def handle_message(bot, websocket):
+    response_as_string = await websocket.recv()
+    try:
+        decoded_response = response_as_string.decode('utf-8').replace("'", '"')
+        response: PlayState = PlayState.from_dict(json.loads(decoded_response))
+        bot_answer = bot.play(response)
+        await websocket.send(json.dumps({"fly": bot_answer}))
+    except UnicodeDecodeError:
+        print("got ping message")
 
-    uri = f"ws://localhost:{port}/{ai_bot.get_name()}"
+
+async def client(bot, port):
+    uri = f"ws://localhost:{port}/{bot.get_name()}"
     async with websockets.connect(uri, ping_timeout=None, ping_interval=None) as websocket:
+        print("Connected to server.")
         while True:
-            response_as_string = await websocket.recv()
             try:
-                decoded_response = response_as_string.decode('utf-8').replace("'", '"')
-                response: PlayState = PlayState.from_dict(json.loads(decoded_response))
-                bot_answer = ai_bot.play(response)
-                await websocket.send(json.dumps({"fly": bot_answer}))
-            except UnicodeDecodeError:
-                print("got ping message")
+                await handle_message(bot, websocket)
+            except websockets.ConnectionClosedOK:
+                print("Connection closed by server.")
+                break
+            except websockets.ConnectionClosedError:
+                print("Server was shut down.")
+                break
 
 
 if __name__ == "__main__":
     ai_bot = ai_factory()
-    port = 5050
+    used_port = 5050
     if len(sys.argv) == 2:
         ai_bot = ai_factory(sys.argv[1])
     if len(sys.argv) == 3:
         ai_bot = ai_factory(sys.argv[1])
-        port = sys.argv[2]
+        used_port = sys.argv[2]
 
-    asyncio.get_event_loop().run_until_complete(client(ai_bot, port))
+    asyncio.run(client(ai_bot, used_port))
