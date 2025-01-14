682f2dd2376255aa171d49edb9881066cad42953
CB_Fabi <fabian@codingbuddies.de>
2025-01-15 00:17:26 +0100

    Add my_ai for python

--- bots/pythonClient/Bots/aiFactory.py
+++ bots/pythonClient/Bots/aiFactory.py
@@ -1,10 +1,12 @@
 from Bots.bot_ai import BotAI
 from Bots.first_ai import FirstAI
+from Bots.my_ai import MyAI
 
 ai_bots = {
-    "FirstAI": FirstAI
+    "FirstAI": FirstAI,
+    "MyAI": MyAI,
 }
 
 
-def ai_factory(bot_selection="FirstAI") -> BotAI:
+def ai_factory(bot_selection="MyAI") -> BotAI:
     return ai_bots[bot_selection]()
---
+++ bots/pythonClient/Bots/my_ai.py
@@ -0,0 +1,17 @@
+from Bots.bot_ai import BotAI
+from Bots.data import PlayState
+
+
+class MyAI(BotAI):
+    fly = True
+
+    def play(self, current_game_state: PlayState):
+        # todo: put your logic code here
+        return self.fly
+
+    def get_name(self):
+        return self.name
+
+    def __init__(self):
+        # todo: give your bot a super duper cool name
+        self.name = "YourBotName"
